6a962b74119fec39ff5907cdfbbf2b4cbf1ee1e0
anthony-lamelas <anthony.lamelas23@gmail.com>
2026-03-29 22:58:20 -0400

    fixed config and requirements

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
@@ -5,6 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
+    ML_API_URL: Optional[str] = None
 
     class Config:
         env_file = ".env"
